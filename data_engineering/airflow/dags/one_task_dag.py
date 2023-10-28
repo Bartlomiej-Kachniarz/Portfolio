d06b25ca8ea76695ed18ebfac2a4913858b711f9
Bartłomiej Kachniarz <bartlomiej.kachniarz01@gmail.com>
2023-10-28 19:46:00 +0200

    one new dag

---
+++ data_engineering/airflow/dags/one_task_dag.py
@@ -0,0 +1,25 @@
+"""Dag doing only one task"""
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "Bart",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "catchup": False,
+    "start_date": datetime(2023, 1, 1),
+}
+
+with DAG(
+    dag_id="one_task_dag",
+    description="Basic DAG implementation with only one task.",
+    schedule_interval=None,
+    default_args=default_args,
+) as dag:
+    task_1 = BashOperator(
+        task_id="one_and_only_task", bash_command='echo "Hello World!" > /temp/hello-world.txt', dag=dag
+    )
